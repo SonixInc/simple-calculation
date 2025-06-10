d0c070cb41d482fd398fcd5af78a3f57ff84a6c0
Danil Lukov <dl@elio-systems.com>
2025-06-10 10:50:28 +0700

    Removed unused classes

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -8,14 +8,10 @@ use App\Service\PurchaseService;
 use App\Struct\CalcPriceStruct;
 use App\Struct\OrderResponseStruct;
 use App\Struct\PurchaseStruct;
-use App\Util\ValidationListParser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * Class CheckoutController
@@ -24,8 +20,6 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class CheckoutController extends AbstractController
 {
     public function __construct(
-        private readonly SerializerInterface $serializer,
-        private readonly ValidatorInterface $validator,
         private readonly PriceService $priceService,
         private readonly PurchaseService $purchaseService
     )
